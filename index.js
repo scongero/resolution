b8525452f799f3741f0ac8da511e0e3e006bc54c
scongero <scongero@ucsd.edu>
2019-04-13 16:03:37 -0700

    small changes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./middleware/logger');
 
 const app = express();
 
-// Set static folder
+// init middleware
+//app.use(logger);
+
+// set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
